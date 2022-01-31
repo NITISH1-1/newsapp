2c0ff85ab387b1f7ff91a10c50d6125b70c5e5dc
Nitish Rao <51140695+NITISH1-1@users.noreply.github.com>
2022-01-31 22:43:03 +0530

    added news component

---
+++ src/components/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
+
+export class News extends Component {
+  render() {
+    return <div>
+      This is news component.
+      <NewsItem/>
+    </div>;
+  }
+}
+
+export default News;
