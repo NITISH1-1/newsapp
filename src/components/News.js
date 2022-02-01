a24563980ab44e95610df8c03bc36b7b4394aa08
Nitish Rao <nitish.rao@unthinlable.co>
2022-02-01 22:46:50 +0530

    integrated api for news

--- src/components/News.js
+++ src/components/News.js
@@ -3,9 +3,21 @@ import NewsItem from './NewsItem';
 
 export class News extends Component {
   render() {
-    return <div>
-      This is news component.
-      <NewsItem/>
+    return <div className='container my-3'>
+      <h2>India News - Top Headlines</h2>
+
+      <div className='row'>
+      <div className='col-md-4'>
+      <NewsItem title="Title" description="My News"/>
+      </div>
+      <div className='col-md-4'>
+      <NewsItem title="Title" description="My News"/>
+      </div>
+      <div className='col-md-4'>
+      <NewsItem title="Title" description="My News"/>
+      </div>
+      <div/>
+    </div>
     </div>;
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 export class NewsItem extends Component {
   render() {
+    let {title, description} = this.props;
     return <div>
-      This is news item.
+      <div className="card" style={{width: '18rem'}}>
+        <img src="https://www.the-sun.com/wp-content/uploads/sites/6/2022/01/BB-CHESLIE-KRYST-LIVE-BLOG.jpg?strip=all&quality=100&w=1200&h=800&crop=1" className="card-img-top" alt="/"/>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
     </div>;
   }
 }
